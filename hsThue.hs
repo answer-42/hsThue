371bf60540a38fa559af296a8a5ef4a1bfdf6475
Sebastian Benque <sebastian.benque@gmail.com>
2013-02-15 09:04:57 +0100

    Now debug flag is working.

--- hsThue.hs
+++ hsThue.hs
@@ -6,10 +6,9 @@ import System.Environment (getArgs)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 
-import Control.Monad (void,foldM)
+import Control.Monad (when,foldM)
 
 {- TODO
- - * add flags
  - * add proper testing
  -}
 
@@ -33,40 +32,38 @@ mkRuleBase x = map (addAnn . twoTuple . T.splitOn delim) $
         twoTuple _     = error "Rulebase is not correct -- mkRuleBase"
 
         addAnn :: (T.Text,T.Text) -> RuleBase
-        addAnn (x,(T.uncons -> Just (y,ys))) | y == outp = (x,ys,Output)
-                                             | otherwise = (x,T.cons y ys,Subst) 
+        addAnn (x,T.uncons -> Just (y,ys)) | y == outp = (x,ys,Output)
+                                           | otherwise = (x,T.cons y ys,Subst) 
 
 mkInitState :: T.Text -> T.Text
-mkInitState = T.unlines . tail . dropWhile (/=delim) . T.lines
+mkInitState = T.strip . T.unlines . tail . dropWhile (/=delim) . T.lines
 
 mkFlag :: T.Text -> Flag
 mkFlag x | x == "d"  = Debug
          | x == "n"  = NoFlag 
          | otherwise = error "Unknown Flag -- mkFlag"
 
-runProg :: Flag -> [RuleBase] -> T.Text -> IO (T.Text)
-runProg _ rB iS = f iS
-  where f iS' = let g a (x,y,fl) = if fl == Output 
-                                   then do let i = T.count x a
-                                               s = T.concat $ replicate i y
-                                           T.putStrLn s
-                                           return $ T.concat $ T.splitOn x a
-                                   else return $ T.replace x y a
-                    iS'' = foldM g iS' rB
-                in do iS''' <- iS''  
-                      if iS''' == iS' then iS'' else f iS''' 
+runProg :: Flag -> [RuleBase] -> T.Text -> IO T.Text
+runProg  fl rB = f
+  where f iS = let g a (x,y,s) = do when (fl == Debug)
+                                         $ T.putStr "DEBUG: " >> T.putStrLn iS
+                                    if s == Output 
+                                    then T.putStrLn (T.concat $ replicate (T.count x a) y)
+                                         >> return (T.concat $ T.splitOn x a)
+                                    else return $ T.replace x y a
+                in do iS' <- foldM g iS rB
+                      if iS' == iS then return iS' else f iS' 
 
 main :: IO ()
 main = do
   args <- getArgs
-  f <- T.readFile $ if length args == 1 then head args else args !! 1
+  f <- T.readFile $ if length args == 1 then head args 
+                    else if length args == 2 then args !! 1
+                    else error "Argument error -- main"
   let rB = mkRuleBase f  -- Rulebase
       iS = mkInitState f -- Initial state 
       -- Flag
       fl = mkFlag $ if length args == 2 then T.pack $ head args else "n"    
 
-  --print rB
-  --print iS
-  --print fl
-  void $ runProg fl rB iS
+  runProg fl rB iS >>= T.putStrLn
 
